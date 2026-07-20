1edfbb95c74e0562e357413120069fb1f6336725
Paul Monson <pmonson711@gmail.com>
2026-07-20 16:42:43 -0500

    feat: add leaf gen_servers (cache, metrics, router, auth, api, logger)

---
+++ examples/demo_app/src/demo_metrics.erl
@@ -0,0 +1,19 @@
+-module(demo_metrics).
+-behaviour(gen_server).
+
+-export([start_link/0, init/1, handle_call/3, handle_cast/2, handle_info/2]).
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+init([]) ->
+    {ok, #{}}.
+
+handle_call(_Req, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Msg, State) ->
+    {noreply, State}.
