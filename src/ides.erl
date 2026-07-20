486add403694a72a288b9b64e36a8604ff534877
Paul Monson <pmonson711@gmail.com>
2026-07-20 12:03:07 -0500

    feat: define ides module API, types, and exports

--- src/ides.erl
+++ src/ides.erl
@@ -1,3 +1,44 @@
 -module(ides).
 
--export([]).
+-export([ancestors/1, format/2, print/2]).
+
+-type supervisor_strategy() :: one_for_one
+                             | one_for_all
+                             | rest_for_one
+                             | simple_one_for_one.
+
+-type child_restart_type() :: permanent
+                             | transient
+                             | temporary.
+
+-type child_process() :: #{
+    name         := string(),
+    pid          := pid(),
+    type         := worker,
+    restart_type := child_restart_type()
+}.
+
+-type supervisor_process() :: #{
+    name     := string(),
+    pid      := pid(),
+    type     := supervisor,
+    strategy := supervisor_strategy(),
+    children := [process()]
+}.
+
+-type process() :: supervisor_process() | child_process().
+
+-export_type([process/0, supervisor_process/0, child_process/0,
+              supervisor_strategy/0, child_restart_type/0]).
+
+-spec ancestors(TargetPid :: pid()) -> {ok, process()} | {error, term()}.
+ancestors(_TargetPid) ->
+    {error, not_implemented}.
+
+-spec format(TargetPid :: pid(), Tree :: process()) -> iolist().
+format(_TargetPid, _Tree) ->
+    [].
+
+-spec print(TargetPid :: pid(), Tree :: process()) -> ok.
+print(TargetPid, Tree) ->
+    io:put_chars(format(TargetPid, Tree)).
--- test/ides_tests.erl
+++ test/ides_tests.erl
@@ -4,3 +4,9 @@
 
 smoke_test() ->
     ?assertEqual(1, 1).
+
+exports_test() ->
+    Expected = [{ancestors,1}, {format,2}, {print,2}],
+    Exports = [E || {Name,_}=E <- ides:module_info(exports), Name =/= module_info],
+    ?assertEqual(lists:sort(Expected),
+                 lists:sort(Exports)).
