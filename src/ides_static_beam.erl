e27f9973879d5dc14f8492737743d69ece1e76f3
Paul Monson <pmonson711@gmail.com>
2026-07-22 12:12:58 -0500

    feat: add ides_static_beam for BEAM file loading

---
+++ src/ides_static_beam.erl
@@ -0,0 +1,74 @@
+-module(ides_static_beam).
+
+-export([load_beams/1, is_supervisor/1]).
+
+-type beam_info() :: #{
+    module := module(),
+    attributes := [{atom(), [term()]}],
+    exports := [{atom(), arity()}],
+    abstract_code => abstr()
+}.
+
+-type abstr() :: [term()].
+
+-export_type([beam_info/0]).
+
+is_supervisor(#{attributes := Attrs}) ->
+    lists:any(
+        fun({behaviour, Behaviours}) ->
+            lists:member(supervisor, Behaviours);
+           (_) ->
+            false
+        end,
+        Attrs
+    ).
+
+load_beams(Paths) ->
+    Results = lists:foldl(fun load_beam/2, #{}, Paths),
+    {ok, Results}.
+
+load_beam(Path, Acc) ->
+    case beam_lib:chunks(Path, [attributes, exports, abstract_code, debug_info]) of
+        {ok, {Module, Chunks}} ->
+            Attrs = proplists:get_value(attributes, Chunks, []),
+            Exports = proplists:get_value(exports, Chunks, []),
+            Info = #{
+                module => Module,
+                attributes => Attrs,
+                exports => Exports
+            },
+            Info2 = case extract_abstract_code(Chunks) of
+                {ok, Code} -> Info#{abstract_code => Code};
+                error -> Info
+            end,
+            Acc#{Module => Info2};
+        _ ->
+            Acc
+    end.
+
+extract_abstract_code(Chunks) ->
+    %% Try abstract_code chunk first
+    case proplists:get_value(abstract_code, Chunks) of
+        {raw_abstract_v1, Code} ->
+            {ok, Code};
+        Code when is_list(Code) ->
+            {ok, Code};
+        _ ->
+            %% Try debug_info chunk
+            case proplists:get_value(debug_info, Chunks) of
+                {erl_abstract_code, Forms} when is_list(Forms) ->
+                    {ok, Forms};
+                {Backend, Metadata} when is_atom(Backend) ->
+                    case proplists:get_value(abstract_code, Metadata) of
+                        {raw_abstract_v1, Code} -> {ok, Code};
+                        _ -> error
+                    end;
+                {_Backend, _Backend2, Metadata} ->
+                    case proplists:get_value(abstract_code, Metadata) of
+                        {raw_abstract_v1, Code} -> {ok, Code};
+                        _ -> error
+                    end;
+                _ ->
+                    error
+            end
+    end.
---
+++ test/ides_static_beam_tests.erl
@@ -0,0 +1,72 @@
+-module(ides_static_beam_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+compile_test_beam(ModuleName, SourceBody) ->
+    SrcFile = filename:join(["test", "support", atom_to_list(ModuleName) ++ ".erl"]),
+    ok = file:write_file(SrcFile, SourceBody),
+    {ok, _Mod, Beam} = compile:file(SrcFile, [debug_info, binary, report, {outdir, "test/support"}]),
+    BeamFile = filename:rootname(SrcFile) ++ ".beam",
+    ok = file:write_file(BeamFile, Beam),
+    BeamFile.
+
+load_supervisor_beam_test() ->
+    BeamFile = compile_test_beam(test_beam_sup,
+        "-module(test_beam_sup).\n"
+        "-behaviour(supervisor).\n"
+        "-export([start_link/0, init/1]).\n"
+        "start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).\n"
+        "init([]) -> {ok, {#{strategy => one_for_one, intensity => 1, period => 5}, []}}.\n"
+    ),
+    {ok, BeamMap} = ides_static_beam:load_beams([BeamFile]),
+    ?assert(maps:is_key(test_beam_sup, BeamMap)),
+    #{test_beam_sup := Info} = BeamMap,
+    ?assertEqual(test_beam_sup, maps:get(module, Info)),
+    ?assert(is_list(maps:get(attributes, Info))),
+    ?assert(is_list(maps:get(exports, Info))),
+    ?assert(is_list(maps:get(abstract_code, Info))).
+
+load_non_supervisor_beam_test() ->
+    BeamFile = compile_test_beam(test_beam_gen,
+        "-module(test_beam_gen).\n"
+        "-behaviour(gen_server).\n"
+        "-export([start_link/0, init/1, handle_call/3, handle_cast/2, handle_info/2]).\n"
+        "start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).\n"
+        "init([]) -> {ok, #{}}.\n"
+        "handle_call(_R, _F, S) -> {reply, ok, S}.\n"
+        "handle_cast(_M, S) -> {noreply, S}.\n"
+        "handle_info(_M, S) -> {noreply, S}.\n"
+    ),
+    {ok, BeamMap} = ides_static_beam:load_beams([BeamFile]),
+    ?assert(maps:is_key(test_beam_gen, BeamMap)).
+
+is_supervisor_test() ->
+    SupCode = compile_test_beam(test_sup_check,
+        "-module(test_sup_check).\n"
+        "-behaviour(supervisor).\n"
+        "-export([start_link/0, init/1]).\n"
+        "start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).\n"
+        "init([]) -> {ok, {#{strategy => one_for_one, intensity => 1, period => 5}, []}}.\n"
+    ),
+    {ok, BeamMap} = ides_static_beam:load_beams([SupCode]),
+    #{test_sup_check := Info} = BeamMap,
+    ?assert(ides_static_beam:is_supervisor(Info)).
+
+is_not_supervisor_test() ->
+    GenCode = compile_test_beam(test_not_sup,
+        "-module(test_not_sup).\n"
+        "-behaviour(gen_server).\n"
+        "-export([start_link/0, init/1, handle_call/3, handle_cast/2, handle_info/2]).\n"
+        "start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).\n"
+        "init([]) -> {ok, #{}}.\n"
+        "handle_call(_R, _F, S) -> {reply, ok, S}.\n"
+        "handle_cast(_M, S) -> {noreply, S}.\n"
+        "handle_info(_M, S) -> {noreply, S}.\n"
+    ),
+    {ok, BeamMap} = ides_static_beam:load_beams([GenCode]),
+    #{test_not_sup := Info} = BeamMap,
+    ?assertNot(ides_static_beam:is_supervisor(Info)).
+
+missing_file_test() ->
+    {ok, BeamMap} = ides_static_beam:load_beams(["nonexistent.beam"]),
+    ?assertEqual(#{}, BeamMap).
